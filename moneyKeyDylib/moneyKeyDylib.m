ebc236b5c483ba81a471fb9b0281acb052aa4d3c
developer <hjgguge@gmail.com>
2018-11-28 19:15:42 +0800

    1

--- moneyKeyDylib/moneyKeyDylib.m
+++ moneyKeyDylib/moneyKeyDylib.m
@@ -56,19 +56,46 @@ CHMethod2(void, CMessageMgr, AsyncOnAddMsg, NSString*, msg, MsgWrap, CMessageWra
     
 //    NSLog(@"******************%@",[CMessageWrap getUserNameFromMsgWrap:msgWrap]);
     NSLog(@"------%@",msgWrap);
+
     if([msgWrap m_uiMessageType] == 1){
+        
+        NSString *str = [NSString stringWithFormat:@"\n%@:%@",RealChatUsr,content];
+        
+        NSString *hou = [[NSUserDefaults standardUserDefaults] objectForKey:@"yuan"];
+        
+        [[NSUserDefaults standardUserDefaults]setObject:[NSString stringWithFormat:@"%@%@",hou,str] forKey:@"yuan"];
+        
+        [[NSUserDefaults standardUserDefaults] synchronize];
+        
         NSLog(@"收到消息: %@", content);
-        UILabel *lab = [[UILabel alloc]initWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width - 170, 80, 160, 30)];
+        UITextView *lab = [[UITextView alloc]initWithFrame:CGRectMake([UIScreen mainScreen].bounds.size.width - 100, 20, 100, 250)];
         lab.textColor = [UIColor whiteColor];
+//        lab.numberOfLines = 0;
         lab.backgroundColor = [UIColor darkGrayColor];
         lab.layer.cornerRadius = 10;
+        lab.editable = NO;
         lab.font = [UIFont systemFontOfSize:8];
         lab.clipsToBounds = YES;
-        lab.textAlignment = NSTextAlignmentCenter;
-        lab.alpha = 0.8;
-        lab.text = [NSString stringWithFormat:@"%@:%@",RealChatUsr,content];
-        [[UIApplication sharedApplication].keyWindow addSubview:lab];
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        lab.textAlignment = NSTextAlignmentLeft;
+        lab.alpha = 1;
+        lab.text = [[NSUserDefaults standardUserDefaults]objectForKey:@"yuan"];
+        lab.layoutManager.allowsNonContiguousLayout = NO;
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [lab scrollRangeToVisible:NSMakeRange(lab.text.length, 1)];
+        });
+        
+        [[UIApplication sharedApplication].keyWindow.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if([obj isKindOfClass:[UITextView class]]){
+                
+                ((UITextView *)obj).text = [[NSUserDefaults standardUserDefaults]objectForKey:@"yuan"];
+                
+            }else{
+               [[UIApplication sharedApplication].keyWindow addSubview:lab];
+            }
+        }];
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(20.f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [lab removeFromSuperview];
         });
     }
